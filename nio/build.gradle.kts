58e5d7ead471d897184403c511ca346c37bc992c
Yura Nosenko <yura@attendify.com>
2020-01-20 22:45:46 +0200

    NIO with copy-pasted blokcing code

--- nio/build.gradle.kts
+++ nio/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.3.RELEASE"
-	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	kotlin("jvm") version "1.3.61"
-	kotlin("plugin.spring") version "1.3.61"
+    id("org.springframework.boot") version "2.2.3.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
 }
 
 group = "com.attendifylabs"
@@ -12,39 +12,42 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
-	implementation("org.springframework.boot.experimental:spring-boot-starter-data-r2dbc")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	runtimeOnly("io.r2dbc:r2dbc-postgresql")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
-	testImplementation("org.springframework.boot.experimental:spring-boot-test-autoconfigure-r2dbc")
-	testImplementation("io.projectreactor:reactor-test")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    val kotlinCoroutinesVersion = "1.3.2"
+    implementation("io.r2dbc:r2dbc-postgresql:0.8.0.RELEASE")
+    implementation("org.springframework.data:spring-data-r2dbc:1.0.0.RELEASE")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$kotlinCoroutinesVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("org.springframework.boot.experimental:spring-boot-test-autoconfigure-r2dbc")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.boot.experimental:spring-boot-bom-r2dbc:0.1.0.M3")
-	}
+    imports {
+        mavenBom("io.r2dbc:r2dbc-bom:Arabba-RC2")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xnew-inference")
+        jvmTarget = "1.8"
+    }
 }
--- nio/src/test/kotlin/com/attendifylabs/nio/NioApplicationTests.kt
+++ nio/src/test/kotlin/com/attendifylabs/nio/NioApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.attendifylabs.nio
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class NioApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//package com.attendifylabs.nio
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class NioApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}
