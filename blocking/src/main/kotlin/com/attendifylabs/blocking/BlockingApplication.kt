e9dfdecc606f341d9398ec366d59cca6793d8767
Yura Nosenko <yura@attendify.com>
2020-02-18 08:15:16 +0200

    For transaction isiolation testing

--- blocking/src/main/kotlin/com/attendifylabs/blocking/BlockingApplication.kt
+++ blocking/src/main/kotlin/com/attendifylabs/blocking/BlockingApplication.kt
@@ -6,11 +6,16 @@ import org.springframework.boot.runApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.transaction.annotation.Isolation
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
+import java.lang.RuntimeException
+import java.lang.Thread.sleep
 
 @SpringBootApplication
 class BlockingApplication {
@@ -24,7 +29,7 @@ fun main() {
 }
 
 @RestController
-@RequestMapping("ramen")
+@RequestMapping("ramen/{order}/{operation}")
 class RamenController(
     private val jdbcTemplate: NamedParameterJdbcTemplate,
     private val restTemplate: RestTemplate,
@@ -32,7 +37,11 @@ class RamenController(
 ) {
 
     @PostMapping
-    fun orderRamen() =
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    fun orderRamen(
+        @PathVariable order: Int,
+        @PathVariable operation: String
+    ) =
         RamenOrder(
             noodles = restTemplate.getForObject(NOODLES_URL),
             broth = restTemplate.getForObject(BROTH_URL),
@@ -42,7 +51,15 @@ class RamenController(
             soySauce = restTemplate.getForObject(SOYSAUSE_URL),
             hotSauce = restTemplate.getForObject(HOTSAUCE_URL)
         ).apply {
-            jdbcTemplate.update(CREATE_RAMEN_ORDER, mapOf(RAMEN_ORDER to objectMapper.writeValueAsString(this)))
+            //jdbcTemplate.update(CREATE_RAMEN_ORDER, mapOf(RAMEN_ORDER to objectMapper.writeValueAsString(this)))
+            if (operation == "delete") {
+                jdbcTemplate.update(DELETE_RAMEN_ORDER, mapOf("order" to order))
+            } else {
+                jdbcTemplate.update(UPDATE_RAMEN_ORDER, mapOf("order" to order))
+                //throw RuntimeException()
+            }
+            //println("Is blocked: order=$order")
+            sleep(10000)
         }
 }
 
@@ -64,6 +81,8 @@ data class RamenOrder(
 )
 
 const val CREATE_RAMEN_ORDER = "INSERT INTO ramen_order(ramen_order) VALUES (to_json(:ramenOrder))"
+const val UPDATE_RAMEN_ORDER = "UPDATE ramen_order SET ramen_order=to_json('{\"order\" : '||:order||'}') WHERE id=6"
+const val DELETE_RAMEN_ORDER = "DELETE FROM ramen_order WHERE id=6"
 const val RAMEN_ORDER = "ramenOrder"
 
 const val NOODLES_URL = "https://blocking.s3.amazonaws.com/noodles.json"
